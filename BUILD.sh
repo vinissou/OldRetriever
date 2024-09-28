42dace0c1c305f70b12af6976ef2d3b38b034c36
Vinícius Souza <vinissou.git@gmail.com>
2024-09-28 16:22:14 -0300

    --

--- BUILD.sh
+++ BUILD.sh
@@ -1,4 +1,5 @@
-#tested on Debian, Alpine Linux and OpenBSD(more on that later) for now
+# tested on Debian, Alpine Linux and OpenBSD(more on that later) for now
+# TODO change to cc on OpenBSD
 dos2unix *
 dos2unix modules/*
 gcc -Wall -Wextra -pedantic -std=c99 -O3 app.c -o retriever
